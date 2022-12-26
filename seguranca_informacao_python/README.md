08c878d378884754de28f4175272f3358e8cafc5
Michelle Cristine Dos Santos <86981990+micvet@users.noreply.github.com>
2022-12-25 23:24:12 -0300

    Delete seguranca_informacao_python directory

--- seguranca_informacao_python/README.md
+++
@@ -1,4 +0,0 @@
-# Segurança da informação com Python
-
-1) O primeiro programa criado foi com o objetivo de testar a conexão com um servidor e  determinada porta. 
-
--- seguranca_informacao_python/cliente_udp.py
+++
@@ -1,3 +0,0 @@
- #udp = user datagram protocol. Parecido com o tcp. Também está na camada de transporte. 
-# Permite o envio de datagramas por pacotes ipv4 ou ipv6, mas sem verificação de integridade, mas sim de disponibilidade. Verifica se o hosp está recebendo dados. 
-# Contempla o princípio de disponibilidade.
